8793fe3c23d8f95b7939652bae49a957af4fe4bd
dawitYenew12 <dawitYenew.12@gmail.com>
2023-06-06 14:25:22 -0700

    minor fix

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for ch in str:
         if ord(ch) not in range(65, 91):
             ch = chr(ord(ch) - 32)
-         print("{:s}".format(ch), end="")
+        print("{:s}".format(ch), end="")
     print()
