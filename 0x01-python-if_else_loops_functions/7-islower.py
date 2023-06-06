4f8a4a34b0edd56418cdf84a0b3791dcc681215d
dawitYenew12 <dawitYenew.12@gmail.com>
2023-06-06 14:16:02 -0700

    function name edited and return value added

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def islowe(c):
+def islower(c):
     if ord(c) in range(97, 123):
         print("{} is lower".format(c))
+        return True
     else:
         print("{} is upper".format(c))
+        return False
