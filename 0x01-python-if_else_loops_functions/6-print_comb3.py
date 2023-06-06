74305e5357376588598dd2ef534d3205ff388e40
dawitYenew12 <dawitYenew.12@gmail.com>
2023-06-06 14:10:54 -0700

    minor fix for task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 for d1 in range(9):
     for d2 in range(d1 + 1, 10):
-        #if it is the last
         if d1 == 8 and d2 == 9:
-            print("{:d}{:d}".format(d1, d2))
+            print("{:d}{:d}".format(d1, d2), end="\n")
         else:
             print("{:d}{:d}".format(d1, d2), end=", ")
